ce0316eec482d5cfcf6bdd2a8ae5ac3975f1df94
Daniel J <danieldrdj01@gmail.com>
2018-01-31 16:08:44 -0900

    Update

--- Coding_Class/JohnsonDaniel_A1_2/src/Binary_To_Decimal.java
+++ Coding_Class/JohnsonDaniel_A1_2/src/Binary_To_Decimal.java
@@ -1,20 +1,32 @@
 import java.util.Scanner;
 
+
 public class Binary_To_Decimal {
 
 
     public static void main(String[] args){
 
+        //Creates new scanner object
         Scanner keyboard = new Scanner(System.in);
 
-        //asks for string input that gets assigned to friendRelative
+        //asks for input of a four digit binary number that gets assigned to a string variable using the keyboard.next listener
         System.out.print("Please enter a 4 digit binary number: ");
         String binaryDigit  = keyboard.next();
         //
-        char bd1 = binaryDigit.charAt(0);
-        char bd2 = binaryDigit.charAt(1);
-        char bd3 = binaryDigit.charAt(2);
-        char bd4 = binaryDigit.charAt(3);
+
+        //Reads the individual characters from strings and converts them to integers of 1 and 0 by subtracting 48.
+        //After converting the characters to 1 and 0, it multiplies them by powers of two to finish the conversion into an integer from a binary number.
+        int bd1 = (binaryDigit.charAt(0) - 48)*8;
+        int bd2 = (binaryDigit.charAt(1) - 48)*4;
+        int bd3 = (binaryDigit.charAt(2) - 48)*2;
+        int bd4 = (binaryDigit.charAt(3) - 48);
+
+
+        //Adds all the converted numbers together to get the final value.
+        int finalV = bd1 + bd2 + bd3 + bd4;
+
+        //Prints the final value.
+        System.out.println(finalV);
 
 
     }
